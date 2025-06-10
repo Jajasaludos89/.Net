ae1f827ade189d23689db9ddbe6e25ddcfa87202
Javier Pilco <brandon.pilco3401@utc.edu.ec>
2025-06-09 19:48:28 -0500

    8 Configuraciones del aspx.cs Javier y Jhonnatan

--- Software/Desarrolladores.aspx.cs
+++ Software/Desarrolladores.aspx.cs
@@ -4,14 +4,131 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace Software
 {
     public partial class Desarrolladores : System.Web.UI.Page
     {
+        string cadenaConexion = ConfigurationManager.ConnectionStrings["conexionSistema"].ConnectionString;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                MostrarDesarrolladores();
+                btnActualizar.Visible = false;
+            }
+        }
 
+        private void MostrarDesarrolladores()
+        {
+            using (SqlConnection conexion = new SqlConnection(cadenaConexion))
+            {
+                SqlCommand cmd = new SqlCommand("ConsultarDesarrolladores", conexion);
+                cmd.CommandType = CommandType.StoredProcedure;
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                gvDesarrolladores.DataSource = dt;
+                gvDesarrolladores.DataBind();
+            }
+        }
+
+        protected void btnGuardar_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conexion = new SqlConnection(cadenaConexion))
+            {
+                SqlCommand cmd = new SqlCommand("InsertarDesarrollador", conexion);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@Nombre", txtNombre.Text);
+                cmd.Parameters.AddWithValue("@Apellido", txtApellido.Text);
+                cmd.Parameters.AddWithValue("@Nacionalidad", txtNacionalidad.Text);
+                cmd.Parameters.AddWithValue("@Especialidad", txtEspecialidad.Text);
+                cmd.Parameters.AddWithValue("@Email", txtEmail.Text);
+
+                conexion.Open();
+                cmd.ExecuteNonQuery();
+                conexion.Close();
+
+                LimpiarCampos();
+                MostrarDesarrolladores();
+            }
+        }
+
+        protected void gvDesarrolladores_RowCommand(object sender, System.Web.UI.WebControls.GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "Editar")
+            {
+                int index = Convert.ToInt32(e.CommandArgument);
+                GridViewRow row = gvDesarrolladores.Rows[index];
+
+                hdfIdDesarrollador.Value = row.Cells[0].Text;
+                txtNombre.Text = row.Cells[1].Text;
+                txtApellido.Text = row.Cells[2].Text;
+                txtNacionalidad.Text = row.Cells[3].Text;
+                txtEspecialidad.Text = row.Cells[4].Text;
+                txtEmail.Text = row.Cells[5].Text;
+
+                btnGuardar.Visible = false;
+                btnActualizar.Visible = true;
+            }
+            else if (e.CommandName == "Eliminar")
+            {
+                int id = Convert.ToInt32(e.CommandArgument);
+
+                using (SqlConnection conexion = new SqlConnection(cadenaConexion))
+                {
+                    SqlCommand cmd = new SqlCommand("EliminarDesarrollador", conexion);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@IdDesarrollador", id);
+
+                    conexion.Open();
+                    cmd.ExecuteNonQuery();
+                    conexion.Close();
+
+                    MostrarDesarrolladores();
+                }
+            }
+        }
+
+        protected void btnActualizar_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conexion = new SqlConnection(cadenaConexion))
+            {
+                SqlCommand cmd = new SqlCommand("ActualizarDesarrollador", conexion);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@IdDesarrollador", Convert.ToInt32(hdfIdDesarrollador.Value));
+                cmd.Parameters.AddWithValue("@Nombre", txtNombre.Text);
+                cmd.Parameters.AddWithValue("@Apellido", txtApellido.Text);
+                cmd.Parameters.AddWithValue("@Nacionalidad", txtNacionalidad.Text);
+                cmd.Parameters.AddWithValue("@Especialidad", txtEspecialidad.Text);
+                cmd.Parameters.AddWithValue("@Email", txtEmail.Text);
+
+                conexion.Open();
+                cmd.ExecuteNonQuery();
+                conexion.Close();
+
+                LimpiarCampos();
+                MostrarDesarrolladores();
+                btnGuardar.Visible = true;
+                btnActualizar.Visible = false;
+            }
+        }
+
+        private void LimpiarCampos()
+        {
+            txtNombre.Text = "";
+            txtApellido.Text = "";
+            txtNacionalidad.Text = "";
+            txtEspecialidad.Text = "";
+            txtEmail.Text = "";
+            hdfIdDesarrollador.Value = "";
         }
     }
 }
